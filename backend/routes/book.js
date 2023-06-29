08d2b8c3f8388322f957c4f822b686442df3f3a3
Charlene Lebreton <shah.lebreton@gmail.com>
2023-06-29 15:57:10 +0200

    route pu and delete

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -7,10 +7,10 @@ const multer = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
-
 router.get('/:id', bookCtrl.getOneBook);
-
 router.post('/', auth, multer, bookCtrl.createBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 
 
 module.exports = router;
